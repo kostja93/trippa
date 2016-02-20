581073127aa40ea6a5adcdb0d68da6674fd861a8
Kostja <k.gontscharow@gmail.com>
2016-02-20 15:40:38 +0100

    google api locations with pictures

--- api/app/GoogleLocationApi.php
+++ api/app/GoogleLocationApi.php
@@ -6,19 +6,67 @@
  * Time: 13:39
  */
 
-namespace app;
+namespace App;
 
 
 class GoogleLocationApi {
-    private $baseUrl =  "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+    private $baseUrl =  "https://maps.googleapis.com/maps/api/place/";
+    private $key;
 
     public function __construct($key) {
-        $url ="&location=41.3,2.1&radius=5000";
-        $this->baseUrl .= "key=" . $key;
+        $this->key = $key;
     }
 
-    public function getLocations($lat, $lon) {
+    public function get($lat, $lon, $radius) {
+        $locations = $this->getLocations($lat,$lon, $radius);
+        return $this->updateLocations($locations);
 
+        return $locations;
+    }
+
+    public function getLocations($lat, $lon, $radius) {
+        $url = $this->baseUrl . "nearbysearch/json?key=" . $this->key . "&location=$lat,$lon&radius=$radius&type=bar";
+
+        $rawData = file_get_contents($url);
+
+        $data = json_decode($rawData);
+
+        if(strtolower($data->status) == "ok") {
+            return $data->results;
+        }
+
+        return [];
+    }
+
+    public function updateLocations($locations) {
+        $results = [];
+        foreach($locations as $location) {
+            $dbLoc = Location::where('lat', $location->geometry->location->lat)
+                ->where('lon', $location->geometry->location->lng)
+                ->where('name', $location->name);
+
+            if($dbLoc->count() == 0) {
+                $newLoc = new Location();
+                $newLoc->name = $location->name;
+
+                if(isset($location->photos[0]->photo_reference))
+                    $newLoc->picture = $this->savePicture($location->photos[0]->photo_reference);
+
+                $newLoc->lat  = $location->geometry->location->lat;
+                $newLoc->lon  = $location->geometry->location->lng;
+
+                $newLoc->save();
+                $results[] = $newLoc;
+            } else {
+                $results[] = $dbLoc->first();
+            }
+        }
+
+        return $results;
+    }
+
+    public function savePicture($ref) {
+        return $this->baseUrl . "photo?maxwidth=400&photoreference=$ref&key=". $this->key;
     }
 
 }
