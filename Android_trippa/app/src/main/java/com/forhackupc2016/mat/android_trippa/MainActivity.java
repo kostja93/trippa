b2a4e5ad63925e32325f664406dde73299024d33
Truesire <truesyev@live.de>
2016-02-20 02:58:16 +0100

    starter activity done

--- Android_trippa/app/src/main/java/com/forhackupc2016/mat/android_trippa/MainActivity.java
+++ Android_trippa/app/src/main/java/com/forhackupc2016/mat/android_trippa/MainActivity.java
@@ -1,7 +1,10 @@
 package com.forhackupc2016.mat.android_trippa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), SetYourDefaultsActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }
