9272c88b1c2df079d1fcade4d77b83af86bbc142
Truesire <truesyev@live.de>
2016-02-20 02:24:06 +0100

    init for android project

---
+++ Android_trippa/app/src/main/java/com/forhackupc2016/mat/android_trippa/MainActivity.java
@@ -0,0 +1,14 @@
+package com.forhackupc2016.mat.android_trippa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
